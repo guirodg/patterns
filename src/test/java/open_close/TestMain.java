4e2f23f2c30e51820bd12f31ec6474f579c6e8e3
Guilherme Rodrigues <guilherme.rodrigues@spcbrasil.org.br>
2022-08-02 09:16:09 -0300

    first commit

---
+++ src/test/java/open_close/TestMain.java
@@ -0,0 +1,74 @@
+package open_close;
+// Open Closed Principle
+// Dependency Inversion
+
+import org.junit.jupiter.api.Test;
+
+interface ValidaInterfaceComun {
+  void valida();
+}
+
+public class TestMain {
+
+  @Test
+  void testOne() {
+    final var validaSenha = new ValidaSenha();
+    final var validaEmail = new ValidaEmail();
+    final var validadorComposite = new ValidadorComposite(validaSenha, validaEmail);
+    final var classConcretaNMuda = new ClassConcretaNMuda(validadorComposite);
+    classConcretaNMuda.add();
+  }
+
+}
+
+class ClassConcretaNMuda {
+  private final ValidaInterfaceComun validaInterfaceComun;
+
+  ClassConcretaNMuda(ValidaInterfaceComun validaInterfaceComun) {
+    this.validaInterfaceComun = validaInterfaceComun;
+  }
+
+  void add() {
+    validaInterfaceComun.valida();
+  }
+}
+
+class ValidaEmail implements ValidaInterfaceComun {
+
+  @Override
+  public void valida() {
+    System.out.println("Validando campo obrigatorio");
+
+  }
+}
+
+class ValidaSenha implements ValidaInterfaceComun {
+
+  @Override
+  public void valida() {
+    System.out.println("Validando Phone");
+
+  }
+}
+
+//Composition Root
+class ValidadorComposite implements ValidaInterfaceComun {
+
+  private final ValidaSenha validaSenha;
+  private final ValidaEmail validaEmail;
+
+  ValidadorComposite(ValidaSenha validaSenha, ValidaEmail validaEmail) {
+    this.validaSenha = validaSenha;
+    this.validaEmail = validaEmail;
+  }
+
+  @Override
+  public void valida() {
+    this.validaSenha.valida();
+    this.validaEmail.valida();
+  }
+}
+
+
+
+
